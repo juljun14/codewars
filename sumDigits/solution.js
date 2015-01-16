906c360ddeb40d060feaa422b7a61268d27a9166
Julius Jung <juljun14@gmail.com>
2015-01-15 22:32:47 -0500

    commit initial solution to sumDigits

--- sumDigits/solution.js
+++ sumDigits/solution.js
@@ -1,3 +1,7 @@
 function sumDigits(number) {
-	
+	return parseInt(Math.abs(number).toString().split("").reduce(function(a,b) {
+		return parseInt(a) + parseInt(b);
+	}));
 }
+
+console.log(typeof parseInt(Math.abs(0).toString().split("").reduce(function(a,b) { return parseInt(a) + parseInt(b);})));
