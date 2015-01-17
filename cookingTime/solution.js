e284e531a3903bcc0c23b1acb9e79a38ad870aa4
Julius Jung <juljun14@gmail.com>
2015-01-17 18:44:14 -0500

    add testing to cookingTime function

---
+++ cookingTime/solution.js
@@ -0,0 +1,18 @@
+function cookingTime(eggs) {
+
+}
+
+// cook time for 0 eggs
+console.log(cookingTime(0) === 0)
+// cook time for 2 eggs
+console.log(cookingTime(2) === 5)
+// cook time for 5 eggs
+console.log(cookingTime(5) === 5)
+// cook time for 8 eggs
+console.log(cookingTime(8) === 5)
+// cook time for 15 eggs
+console.log(cookingTime(15) === 10)
+// cook time for 16 eggs
+console.log(cookingTime(16) === 10)
+// cook time for 20 eggs
+console.log(cookingTime(20) === 15)
