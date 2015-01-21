ddf088d7dd99f43cd42d05fe5564389cd3139c08
Julius Jung <juljun14@gmail.com>
2015-01-20 23:51:08 -0500

    delete testing because the '===' operand will return false for not referencing the same object although the arrays contain the same elements

--- getAttendees/solution.js
+++ getAttendees/solution.js
@@ -9,15 +9,3 @@ function getAttendees(peopleInvited, responses){
   return peopleInvited;
 
 }
-
-// test
-var people = ['Easter Bunny', 'Tooth Fairy', 'Frosty the Snowman', 
-              'Jack Frost', 'Cupid', 'Father Time'];
-var responses = [ 
-     {name: 'Easter Bunny', response: 'declined'}, 
-     {name: 'Jack Frost', response: 'declined'}, 
-     {name: 'Tooth Fairy', response: 'accepted'} 
-   ]
-
-console.log(getAttendees(people, responses) == ['Tooth Fairy', 'Frosty the Snowman', 'Cupid', 'Father Time'])
-
