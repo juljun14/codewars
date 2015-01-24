0c08522b5c939efe3cdf1204ce6ee09f5c5ef736
Julius Jung <juljun14@gmail.com>
2015-01-23 23:50:09 -0500

    rework solution for more clean and easier to understand code

--- defaultArguments/solution.js
+++ defaultArguments/solution.js
@@ -1,32 +1,28 @@
 function defaultArguments(func, params) {
-  var replaceVar = func.toString().split("return");
-
-  for (var variables in params) {
-  	if (Object.keys(params)[Object.keys(params).length-1] === variables) {
-  		replaceVar.splice(1, 1, "\n"+variables+" = typeof "+variables+" !== 'undefined' ? "+variables+" : "+params[variables]+";\n return"+replaceVar[1]+"")
-  	} else {
-  		replaceVar.splice(1, 0, "\n"+variables+" = typeof "+variables+" !== 'undefined' ? "+variables+" : "+params[variables]+";\n return")
-  	}
+  var reg = /\(([\s\S]*?)\)/;
+  var extractVar = reg.exec(func);
+  if (extractVar) {
+  	var arguments = extractVar[1].split(',');
   }
 
-  var parseFunc = "var restoreFunc = " + replaceVar.join("");
-  eval(parseFunc);
-
-  console.log(parseFunc)
-
-  return restoreFunc;
+  for (i = 0; i < arguments.length; i++) {
+  	if (params[arguments[i]]) {
+  		arguments[i] = params[arguments[i]];
+  	}
+  }
+  console.log(extractVar)
 }
 
-function add(a,b) { return a+b;};
+function add(a,b) { return a+b; };
 
 var add_ = defaultArguments(add,{b:9});
-console.log(add_(10)); // returns 19
-console.log(add_(10,7)); // returns 17
-console.log(add_()); // returns NaN
+// console.log(add_(10)); // returns 19
+// console.log(add_(10,7)); // returns 17
+// console.log(add_()); // returns NaN
 
-add_ = defaultArguments(add_,{b:3, a:2});
-console.log(add_(10)); // returns 13 now
-console.log(add_()); // returns 5
+// add_ = defaultArguments(add_,{b:3, a:2});
+// console.log(add_(10)); // returns 13 now
+// console.log(add_()); // returns 5
 
 // add_ = defaultArguments(add_,{c:3}); // doesn't do anything, since c isn't an argument
 // console.log(add_(10)); // returns NaN
