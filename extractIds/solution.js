c0505793df1c703762e2b0a7333e506947fb6d15
Julius Jung <juljun14@gmail.com>
2015-01-24 23:24:08 -0500

    get working solution but need to understand it

--- extractIds/solution.js
+++ extractIds/solution.js
@@ -1,52 +1,5 @@
-data = {
-    id: 1,
-    items: [{
-        id: 2,
-        items: [{
-              id: 3,
-              items: [
-                {id: 4},
-                {id: 5}
-              ]
-            },{
-              id: 6,
-              items: [{id: 7}]
-            }]
-      }]
+function extractIds(data){
+  return [].concat.apply([], Object.keys(data).map(function(item) {
+    return item === 'id' ? data[item] : extractIds(data[item]);
+  }));
 }
-
-var answer = [];
-var beforeKick;
-
-function initializer() {
-  if (!!data.id) {
-    answer.push(data.id)
-  }
-  if (!!data.items) {
-    data = data.items
-  }
-}
-
-function inception() {
-  for (var levels in data) {
-    if (!!data[levels].id) {
-      answer.push(data[levels].id);
-    }
-    if (!!data[levels].items && levels == 0) {
-      beforeKick = data;
-      data = data[levels].items;
-      inception();
-    } else if (!!beforeKick[levels].items && levels == 1) {
-      answer.push(beforeKick[levels].id)
-      data = beforeKick[levels].items;
-      inception();
-    } else {
-      break;
-    }
-  }
-}
-
-initializer();
-inception();
-
-console.log(answer)
