11a8ae47e17394bf1da8926f4132e1ef121437cd
Julius Jung <juljun14@gmail.com>
2015-06-19 09:13:19 +0700

    commit initial solution with 'best' solution commented out

--- reverseIt/solution.js
+++ reverseIt/solution.js
@@ -1,3 +1,20 @@
 function reverseIt(data){
-
+  if (typeof data == 'string') {
+    return ("" + data).split("").reverse().join("");
+  } else if (typeof data == 'number') {
+    return parseFloat(("" + data).split("").reverse().join(""));
+  } else {
+    return data;
+  }
  }
+
+// function reverseIt(data){
+//   if(typeof data === 'string'){
+//     return data.split("").reverse().join("");
+//   }
+//   if(typeof data === 'number'){
+//     var a = +(data.toString().split("").reverse().join(""));
+//     return a;
+//   }
+//   return data;
+// }
